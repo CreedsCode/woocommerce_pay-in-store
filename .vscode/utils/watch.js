055bc071f77753c16906106cf513376d989b53db
Dercio H <hoffmann.dercio@hotmail.com>
2021-06-26 22:54:46 +0200

    Delete .vscode directory

--- .vscode/utils/watch.js
+++
@@ -1,6 +0,0 @@
-var fs = require("fs");
-const { exec, spawn } = require("child_process");
-
-fs.watch("../../",{recursive: true}, async function(evt, name) {
-  spawn("rsync", ["-avu", '--exclude=".*"', "--delete","../../","../../../wp-content/plugins/woocommerce_pay-in-store"]);
-});
